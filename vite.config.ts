aef296bee5d47cc117b8f8ec315f8e90305753bf
chendiyu <cdy1998cdy@163.com>
2025-03-09 12:29:06 +0800

    Keep calm and commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filenameNew = fileURLToPath(import.meta.url);
+const __dirnameNew = path.dirname(__filenameNew);
+const resolve = (dir: string) => path.join(__dirnameNew, dir);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    hmr: {
+      overlay: false
+    },
+    host: "0.0.0.0",
+    port: 8080, //端口号
+    proxy: {},
+    preTransformRequests: false
+  },
+  resolve: {
+    alias: {
+      "@": resolve("src")
+      // 'vue$': 'vue/dist/vue.esm.js'
+    },
+    dedupe: ["vue"]
+  }
+});
